3e3103073441fe3eb4f8e84d208e6ff5b1195096
arminsultana <arminsultana556@gmail.com>
2022-09-08 16:39:53 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stwaq9t.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function  run(){
+    try{
+
+        await client.connect();
+        console.log('database connected');
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Library!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Library app listening on port ${port}`)
 })
